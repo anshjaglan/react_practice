c9078682ca762124f1b6ecf5a92cb59f7ff7c09e
Ansh Jaglan <anshjaglan2005@gmail.com>
2026-08-06 19:49:44 +0530

    forms

---
+++ 04-forms/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+
+
+
+function App(){
+  // create mutiple states
+  const[name, setName] = useState("");
+  const[age, setAge] = useState("");
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+  function handleSubmit(e){
+  e.preventDefault(); // prevent refresh
+  alert(`Welcome  ${name}`)
+}
+  return(
+    <form onSubmit={handleSubmit}>
+      <h1>React Forms</h1>
+      {/* take input*/}
+       { /* controlled component */}
+      <input 
+      placeholder="Enter Name"
+      type="text"
+    
+      value={name}
+      onChange={(e) => setName(e.target.value)} 
+      />
+     
+      <input 
+      placeholder="Enter Age"
+      type="number"
+      value={age}
+      onChange={(e) => setAge(e.target.value)} 
+      />
+      <input
+      placeholder="Enter Email"
+      type="text"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+      placeholder="Enter Password"
+      type="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      />
+     
+     {/* Conditional rendring*/}
+     <h2>Hello : {name  || "Guest"}  </h2>
+     <h2>Age : {age || "Guessing"}  </h2>
+     <h3>Email : {email || "example@gmail.com"}</h3>
+
+   { /* password masking*/}
+    <h3>Password : {"*".repeat(password.length)}</h3>
+         <button type="submit">
+      Login
+     </button>
+    </form>
+  )
+}
+
+
+export default App;
