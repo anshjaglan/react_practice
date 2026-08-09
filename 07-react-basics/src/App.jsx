77b93bae48b73eef3651f5615101e5e3734c895d
Ansh Jaglan <anshjaglan2005@gmail.com>
2026-08-09 21:48:07 +0530

    uses of prop

--- 07-react-basics/src/App.jsx
+++ 07-react-basics/src/App.jsx
@@ -1,122 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// function App(){
+//   return (
+//     <div>
+//       <h1>My App</h1>
 
-function App() {
-  const [count, setCount] = useState(0)
+//       <User name="Ansh" course="B.Tech CSE" age={21} city="Roorkee" />
+//       <User name="Avi" course="Class 10" age={16} city="Shamli"/>
+     
+//     </div>
+//   );
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+// }
+// function User({name, course, age, city}){
+//   return(
+//     <div>
+//       <h2>{name}</h2>
+//       <h3>{course}</h3>
+//       <p>{age}</p>
+//       <p>{city}</p>
+      
+//     </div>
+//   )
+// };
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+
+
+// export default App;
+
+
+const user =[
+  {
+    name : "Ansh",
+    course : "B.Tech CSE",
+    age : 21,
+    city : "shamli"
+
+  },
+];
+
+function App(){
+  return(
+    <div>
+      <h1>Users</h1>
+      {users.map((user, index) => 
+      <User 
+          key={index}
+          name={user.name}
+          course={user.course}
+          city={user.city}
+          age={user.age}
+      />
+      )}
+    </div>
+  );
 }
 
-export default App
+function User({name, course, age, city}) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Course : {course}</p>
+      <p>Age : {age}</p>
+      <p>City : {city}</p>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
