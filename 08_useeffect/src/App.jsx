0b217e7a05a39d2fdea6822d2eded758ae564a86
Ansh Jaglan <anshjaglan2005@gmail.com>
2026-08-16 23:42:12 +0530

    update app

--- 08_useeffect/src/App.jsx
+++ 08_useeffect/src/App.jsx
@@ -2,18 +2,38 @@ import { useEffect, useState } from "react";
 
 function App(){
   const[count, setCount] = useState(0);
+  const[user, setUser] = useState([]);
   useEffect(() => {
     console.log("const chnaged")
+  },[])
+  useEffect(() => {fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => {
+    return response.json()})
+    .then(data => {
+     
+      // console.log(data);
+      setUser(data);
+    })
   },[])
 
   return(
     <div>
-      <h1>useEffect Ptractice</h1>
+      {/* <h1>useEffect Ptractice</h1>
       <h2>{count}</h2>
 
       <button onClick={() => setCount(count+1)}>
       Increase
-      </button>
+      </button> */}
+      <h1>User</h1>
+      {user.map((item) => (
+        <div>
+          <h2>{item.name}</h2>
+          <h3>{item.username}</h3>
+          <p>{item.address.city}</p>
+      {/* <button onClick={() => setUser(user)}> user details</button> */}
+        </div>
+      ))}
+      
     </div>
   );
 }
